663f98d647a70ad46581784ca735f25e1009dcdc
Maya <79027972026@yandex.ru>
2022-07-08 13:48:12 +0500

    add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {store} from "./BLL (business logic layer)/store";
 import {Provider} from "react-redux";
 
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter >
           <Provider store={store}>
           <App/>
           </Provider>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
 
